d3f9a18caedb8bf2021973b648312be77cd97d43
Roman Tsukanov <roman@tsukanov.me>
2017-03-15 01:22:59 +0100

    Insert summits

---
+++ admin/sql/insert_summits.sql
@@ -0,0 +1,54 @@
+BEGIN;
+
+-- 1 point
+INSERT INTO summit (id, name, points) VALUES (1, 'Molins de Rei-Barcelona', 1);
+INSERT INTO summit (id, name, points) VALUES (2, 'Begues', 1);
+INSERT INTO summit (id, name, points) VALUES (3, 'Ulastrell', 1);
+INSERT INTO summit (id, name, points) VALUES (4, 'Tibidabo', 1);
+INSERT INTO summit (id, name, points) VALUES (5, 'Forat del Vent', 1);
+INSERT INTO summit (id, name, points) VALUES (6, 'Parpers', 1);
+INSERT INTO summit (id, name, points) VALUES (7, 'Conreria', 1);
+INSERT INTO summit (id, name, points) VALUES (8, 'Vallromanes', 1);
+INSERT INTO summit (id, name, points) VALUES (9, 'Montmany', 1);
+INSERT INTO summit (id, name, points) VALUES (10, 'Can Bordoi', 1);
+
+-- 2 points
+INSERT INTO summit (id, name, points) VALUES (11, 'Coll dEstenalles', 2);
+INSERT INTO summit (id, name, points) VALUES (12, 'Orrius', 2);
+INSERT INTO summit (id, name, points) VALUES (13, 'Pollastre', 2);
+INSERT INTO summit (id, name, points) VALUES (14, 'Farell', 2);
+INSERT INTO summit (id, name, points) VALUES (15, 'Costes del Garraf', 2);
+INSERT INTO summit (id, name, points) VALUES (16, 'Rierada', 2);
+INSERT INTO summit (id, name, points) VALUES (17, 'Coll de les Ventoses', 2);
+INSERT INTO summit (id, name, points) VALUES (18, 'Creu Aragall', 2);
+INSERT INTO summit (id, name, points) VALUES (19, 'Coll de Capafonts', 2);
+INSERT INTO summit (id, name, points) VALUES (20, 'Tossa de Mar - Sant Feliu de Guixols', 2);
+
+-- 3 points
+INSERT INTO summit (id, name, points) VALUES (21, 'Serratto del Garraf', 3);
+INSERT INTO summit (id, name, points) VALUES (22, 'Coll de la Creueta', 3);
+INSERT INTO summit (id, name, points) VALUES (23, 'St Llorenc Savall - St Feliu de Codinas', 3);
+INSERT INTO summit (id, name, points) VALUES (24, 'Montserrat', 3);
+INSERT INTO summit (id, name, points) VALUES (25, 'Sant Miquel del Fai', 3);
+INSERT INTO summit (id, name, points) VALUES (26, 'Collada de Toses', 3);
+INSERT INTO summit (id, name, points) VALUES (27, 'Cavall dAcer', 3);
+INSERT INTO summit (id, name, points) VALUES (28, 'Fontpineda', 3);
+INSERT INTO summit (id, name, points) VALUES (29, 'Collformic', 3);
+INSERT INTO summit (id, name, points) VALUES (30, 'Sant Pere de Rhode', 3);
+INSERT INTO summit (id, name, points) VALUES (31, 'Collada de Collfred', 3);
+INSERT INTO summit (id, name, points) VALUES (32, 'Mare de Deu del Mont', 3);
+
+-- 5 points
+INSERT INTO summit (id, name, points) VALUES (33, 'Atajo Farell', 5);
+INSERT INTO summit (id, name, points) VALUES (34, 'Rat Penat', 5);
+INSERT INTO summit (id, name, points) VALUES (35, 'Turo de lHome', 5);
+INSERT INTO summit (id, name, points) VALUES (36, 'Rocacorba', 5);
+INSERT INTO summit (id, name, points) VALUES (37, 'Santuari de Bellmunt', 5);
+INSERT INTO summit (id, name, points) VALUES (38, 'Coll de la Gallina', 5);
+INSERT INTO summit (id, name, points) VALUES (39, 'Port del Canto', 5);
+INSERT INTO summit (id, name, points) VALUES (40, 'Parking Tibidabo', 5);
+INSERT INTO summit (id, name, points) VALUES (41, 'Envalira', 5);
+INSERT INTO summit (id, name, points) VALUES (42, 'Rasos de Peguera', 5);
+INSERT INTO summit (id, name, points) VALUES (43, 'Mont Caro', 5);
+
+COMMIT;
